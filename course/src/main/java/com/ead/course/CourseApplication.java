f1b9bbc74e36e97e134371b9932a1c11f9bb62d4
Rodrigo França <rfrancacosta@gmail.com>
2023-07-29 19:57:43 -0300

    Log4J2
    Testando com Spring

--- course/src/main/java/com/ead/course/CourseApplication.java
+++ course/src/main/java/com/ead/course/CourseApplication.java
@@ -1,13 +1,14 @@
 package com.ead.course;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CourseApplication {
-    private static final Logger logger = (Logger) LoggerFactory.getLogger(CourseApplication.class);
+    private static final Logger logger = LogManager.getLogger(CourseApplication.class);
 
     public static void main(String[] args) {
         logger.info("OLA LOGGER");
